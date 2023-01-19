5f99e60b4d32220e051ec4db338e406daf5018c6
Muhammad Zohaib Mohsin <zohaibmohsin30@gmail.com>
2023-01-19 22:08:22 +0500

    Almost work done

---
+++ pages/posts/first-post.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+ 
+ export default function firstPost() {
+  const YourComponent = () => (
+    <Image
+      src="/Images/profile.jpeg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={150} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  );
+   return (
+     <>
+     <Head>
+      <title>first-post</title>
+     </Head>
+     <Script
+     src="https://connect.facebook.net/en_US/sdk.js"
+     strategy="lazyOnload"
+     onLoad={() =>
+       console.log(`script loaded correctly, window.FB has been populated`)}
+     />
+
+     
+     <h1>First Post</h1>
+     <h3><Link href="/">Go back</Link></h3>
+     <YourComponent />
+     </>
+   )
+ }
+ 
