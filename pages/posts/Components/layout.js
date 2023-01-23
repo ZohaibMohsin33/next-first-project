3de35bd35fa07d75179da2db00143ba3c7ff3c58
Muhammad Zohaib Mohsin <zohaibmohsin30@gmail.com>
2023-01-23 08:11:15 +0500

    project in progress

--- pages/posts/Components/layout.js
+++ pages/posts/Components/layout.js
@@ -4,3 +4,5 @@ import styles from "./layout.module.css";
 export default function Layout({ children }) {
     return <div className={styles.container}>{children}</div>;
   }
+
+  //Picture not showing down
